e57a3e4fcb91f5f258d6c54545162a1b713997fe
Arash Tabatabaei <153722318+arashtabaa@users.noreply.github.com>
2024-06-05 19:48:01 +0330

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace ATM_Queue
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.buttonEnqueue = new System.Windows.Forms.Button();
+            this.buttonDequeue = new System.Windows.Forms.Button();
+            this.buttonFront = new System.Windows.Forms.Button();
+            this.buttonRear = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.pb3 = new System.Windows.Forms.PictureBox();
+            this.pb2 = new System.Windows.Forms.PictureBox();
+            this.pb1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(6, 31);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(280, 28);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(6, 28);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(731, 53);
+            this.flowLayoutPanel1.TabIndex = 6;
+            // 
+            // buttonEnqueue
+            // 
+            this.buttonEnqueue.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonEnqueue.Location = new System.Drawing.Point(6, 80);
+            this.buttonEnqueue.Name = "buttonEnqueue";
+            this.buttonEnqueue.Size = new System.Drawing.Size(280, 67);
+            this.buttonEnqueue.TabIndex = 7;
+            this.buttonEnqueue.Text = "Enqueue";
+            this.buttonEnqueue.UseVisualStyleBackColor = true;
+            this.buttonEnqueue.Click += new System.EventHandler(this.ButtonEnqueue_Click);
+            // 
+            // buttonDequeue
+            // 
+            this.buttonDequeue.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonDequeue.Location = new System.Drawing.Point(6, 153);
+            this.buttonDequeue.Name = "buttonDequeue";
+            this.buttonDequeue.Size = new System.Drawing.Size(280, 67);
+            this.buttonDequeue.TabIndex = 8;
+            this.buttonDequeue.Text = "Dequeue";
+            this.buttonDequeue.UseVisualStyleBackColor = true;
+            this.buttonDequeue.Click += new System.EventHandler(this.ButtonDequeue_Click);
+            // 
+            // buttonFront
+            // 
+            this.buttonFront.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonFront.Location = new System.Drawing.Point(6, 240);
+            this.buttonFront.Name = "buttonFront";
+            this.buttonFront.Size = new System.Drawing.Size(137, 112);
+            this.buttonFront.TabIndex = 9;
+            this.buttonFront.Text = "Front";
+            this.buttonFront.UseVisualStyleBackColor = true;
+            // 
+            // buttonRear
+            // 
+            this.buttonRear.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonRear.Location = new System.Drawing.Point(149, 240);
+            this.buttonRear.Name = "buttonRear";
+            this.buttonRear.Size = new System.Drawing.Size(137, 112);
+            this.buttonRear.TabIndex = 10;
+            this.buttonRear.Text = "Rear";
+            this.buttonRear.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.progressBar1);
+            this.groupBox1.Controls.Add(this.buttonRear);
+            this.groupBox1.Controls.Add(this.buttonEnqueue);
+            this.groupBox1.Controls.Add(this.buttonFront);
+            this.groupBox1.Controls.Add(this.buttonDequeue);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(489, 34);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(292, 357);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Actions";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.flowLayoutPanel1);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(34, 397);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(747, 93);
+            this.groupBox2.TabIndex = 12;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "ATM Queue";
+            // 
+            // pb3
+            // 
+            this.pb3.Location = new System.Drawing.Point(87, 162);
+            this.pb3.Name = "pb3";
+            this.pb3.Size = new System.Drawing.Size(75, 180);
+            this.pb3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb3.TabIndex = 15;
+            this.pb3.TabStop = false;
+            this.pb3.Visible = false;
+            // 
+            // pb2
+            // 
+            this.pb2.Location = new System.Drawing.Point(163, 134);
+            this.pb2.Name = "pb2";
+            this.pb2.Size = new System.Drawing.Size(74, 180);
+            this.pb2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb2.TabIndex = 14;
+            this.pb2.TabStop = false;
+            this.pb2.Visible = false;
+            // 
+            // pb1
+            // 
+            this.pb1.BackColor = System.Drawing.Color.Transparent;
+            this.pb1.Location = new System.Drawing.Point(238, 100);
+            this.pb1.Name = "pb1";
+            this.pb1.Size = new System.Drawing.Size(73, 180);
+            this.pb1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb1.TabIndex = 13;
+            this.pb1.TabStop = false;
+            this.pb1.Visible = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ATM_Queue.Properties.Resources.ATM_T;
+            this.pictureBox1.Location = new System.Drawing.Point(317, 34);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(96, 207);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(229)))), ((int)(((byte)(236)))), ((int)(((byte)(244)))));
+            this.ClientSize = new System.Drawing.Size(794, 501);
+            this.Controls.Add(this.pb3);
+            this.Controls.Add(this.pb2);
+            this.Controls.Add(this.pb1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ATM Queue";
+            this.TransparencyKey = System.Drawing.Color.White;
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pb3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button buttonEnqueue;
+        private System.Windows.Forms.Button buttonDequeue;
+        private System.Windows.Forms.Button buttonFront;
+        private System.Windows.Forms.Button buttonRear;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.PictureBox pb3;
+        private System.Windows.Forms.PictureBox pb2;
+        private System.Windows.Forms.PictureBox pb1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using ATM_Queue.Properties;
+
+namespace ATM_Queue
+{
+    public partial class Form1 : Form
+    {
+        // تعریف یک صف برای نگهداری افراد در صف
+        private readonly Queue<int> _queue = new();
+        // لیستی از تصاویر برای نمایش افراد در صف
+        private readonly List<Bitmap> _images = new() { Resources.Person1, Resources.Person2, Resources.Person3};
+        // آرایه‌ای از PictureBox ها برای نمایش تصاویر افراد
+        private readonly PictureBox[] _pictureBoxes;
+        // شمارنده‌های داخلی برای مدیریت صف
+        private int _personCount = 1, _front = 0, _rear = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            // گرفتن PictureBox ها از فرم و ذخیره در آرایه
+            _pictureBoxes = GetPictureBoxes();
+        }
+
+        private void ButtonEnqueue_Click(object sender, EventArgs e)
+        {
+            // بررسی پر بودن صف
+            if (_queue.Count >= _pictureBoxes.Length)
+            {
+                MessageBox.Show("Queue is full!");
+                return;
+            }
+
+            // اضافه کردن فرد به صف
+            _queue.Enqueue(_personCount);
+            _rear++;
+            // اضافه کردن فرد به FlowLayoutPanel
+            AddPersonToFlowLayout(_personCount.ToString());
+            _personCount++;
+
+            // به‌روزرسانی نوار پیشرفت، نمایش‌ها و برچسب‌ها
+            UpdateProgressBar();
+            UpdateVisuals();
+            UpdateFrontRearLabels();
+        }
+
+        private void ButtonDequeue_Click(object sender, EventArgs e)
+        {
+            // بررسی خالی بودن صف
+            if (_queue.Count == 0)
+            {
+                MessageBox.Show("Queue is empty!");
+                return;
+            }
+
+            // حذف فرد از صف
+            _queue.Dequeue();
+            _front++;
+            // حذف اولین برچسب از FlowLayoutPanel
+            flowLayoutPanel1.Controls.RemoveAt(0);
+
+            // جابه‌جایی تصاویر به راست و به‌روزرسانی نمایش‌ها و برچسب‌ها
+            ShiftToRight();
+            UpdateProgressBar();
+            UpdateVisuals();
+            UpdateFrontRearLabels();
+        }
+
+        private void AddPersonToFlowLayout(string index)
+        {
+            // اضافه کردن برچسب فرد به FlowLayoutPanel
+            flowLayoutPanel1.Controls.Add(new Label
+            {
+                Text = $"<--| \U0001f9cd Person # {index} |",
+                AutoSize = true
+            });
+        }
+
+        private void ShiftToRight()
+        {
+            // جابه‌جایی تصویر اول به انتهای لیست
+            var firstImage = _images[0];
+            _images.RemoveAt(0);
+            _images.Add(firstImage);
+        }
+
+        private void UpdateVisuals()
+        {
+            // به‌روزرسانی تصاویر و مرئی بودن PictureBox ها بر اساس تعداد افراد در صف
+            for (int i = 0; i < _pictureBoxes.Length; i++)
+            {
+                _pictureBoxes[i].Image = i < _images.Count ? _images[i] : null;
+                _pictureBoxes[i].Visible = i < _queue.Count;
+            }
+        }
+
+        private void UpdateFrontRearLabels()
+        {
+            // به‌روزرسانی برچسب‌های Front و Rear
+            buttonFront.Text = $"Front = {_front}";
+            buttonRear.Text = $"Rear = {_rear}";
+        }
+
+        private PictureBox[] GetPictureBoxes() => new[] { pb1, pb2, pb3 };
+
+        private void UpdateProgressBar()
+        {
+            // محاسبه درصد بر اساس تعداد افراد در صف و طول PictureBox ها
+            int percent = (_queue.Count * 100) / _pictureBoxes.Length;
+            progressBar1.Value = percent;
+        }
+    }
+}
